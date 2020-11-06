acbc8f64e8ca6cc37247fb398d987a3a374b5a81
149203 <149203@gmail.com>
2020-11-05 17:02:29 -0800

    complete sum-multiplier

--- sum-multiplier.js
+++ sum-multiplier.js
@@ -1,6 +1,14 @@
 function SumMultiplier(arr) {
    // code goes here
-   return arr;
+
+   // get sum of all elements in arr
+   // sort to get the two highest numbers
+   const sum = arr.reduce((a, b) => a + b, 0);
+   const sortedArrDesc = arr.sort((a, b) => b - a);
+   const num1 = sortedArrDesc[0] || 1; // to handle arrays with only 1 num
+   const num2 = sortedArrDesc[1] || 1;
+
+   return String(num1 * num2 > sum * 2);
 }
 
 // keep this function call here
